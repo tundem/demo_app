84ae66d269213717cd75435ceda9e1eff0fee9eb
Charles Madarikan <tundemadarikan@yahoo.com>
2014-10-01 11:33:49 -0400

    Initial Commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,4 @@
 class Micropost < ActiveRecord::Base
-	params.require(:user).permit(:content, :user_id)
 	belongs_to :user
 	validates :content, :length => { :maximum => 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ActiveRecord::Base
-	params.require(:user).permit(attr_accessible :email, :name)
 	has_many :microposts
 end
