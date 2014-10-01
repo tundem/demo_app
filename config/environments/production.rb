e9b05c4c4073370f3858dcff11a2aa6489eccb23
Charles Madarikan <tundemadarikan@yahoo.com>
2014-10-01 11:57:03 -0400

    Initial Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-DemoApp::application.configure do
+DemoApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
