4615a7f6957d732f92e86c2bcc407b1c54bd3415
Charles Madarikan <tundemadarikan@yahoo.com>
2014-10-01 11:21:00 -0400

    Initial Commit

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
-	attr_accessible :content, :user_id
+	params.require(:user).permit(:content, :user_id)
 	belongs_to :user
 	validates :content, :length => { :maximum => 140 }
 end
