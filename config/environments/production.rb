59686bfd8e329979eda2af8a11aff422b9fb4709
Charles Madarikan <tundemadarikan@yahoo.com>
2014-10-01 12:44:12 -0400

    Initial Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,14 +21,14 @@ DemoApp::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
-  config.assets.compile = true
+  
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
